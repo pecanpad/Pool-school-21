29191e23197c1dcb2f5710c9913fb3e02edc7554
1 <a1@MacBook-Pro-7.local>
2025-05-31 17:39:20 +0300

    true

---
+++ T01D01-1/src/Quest3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mv door_management_fi door_management_files
+
+mkdir door_configuration
+mv *.conf door_configuration/
+
+mkdir door_logs
+mv *.log door_logs/
+
+mkdir door_map
+mv *.1 door_map
---
+++ T01D01-1/src/Quest4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pkill ai_door_control
